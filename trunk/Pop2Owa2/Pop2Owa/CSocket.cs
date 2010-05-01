6bfaab7722738772d412cdb29d529e66c95999bb
idcarlos <idcarlos@d26af7a7-9e0d-0410-be35-ff0fea650fe8>
2010-05-01 08:02:30 +0000

    Initial Release

---
+++ trunk/Pop2Owa2/Pop2Owa/CSocket.cs
@@ -0,0 +1,184 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Carlos
+ * Date: 18/04/2010
+ * Time: 10:43
+ * 
+ */
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+using NLog;
+
+namespace Pop2Owa
+{
+	/// <summary>
+	/// Description of CSocket.
+	/// </summary>
+	public class CSocket: IDisposable
+	{
+		internal event DataArrivalEventHandler DataArrival;
+		internal event ConnectionRequestEventHandler ConnectionRequest; 
+		internal event ConnectionClosedEventHandler ConnectionClosed; 
+        internal delegate void DataArrivalEventHandler(CSocket sender);
+        internal delegate void ConnectionRequestEventHandler(CSocket sender);
+        internal delegate void ConnectionClosedEventHandler(CSocket sender);
+
+        internal string Buffer="";
+		
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+		private Socket m_socListener;
+		private AsyncCallback pfnWorkerCallBack ;
+		private Socket m_socWorker;
+		private bool disposed = false;
+		
+		private class CSocketPacket
+		{
+			public System.Net.Sockets.Socket thisSocket;
+			public byte[] dataBuffer = new byte[1];
+		}		
+		public CSocket(IPAddress address, int port) {
+			//create the listening socket...
+			m_socListener = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+			m_socListener.SetSocketOption(SocketOptionLevel.Socket,SocketOptionName.ReuseAddress, true);
+			IPEndPoint ipLocal = new IPEndPoint (address, port);
+			//bind to local IP Address...
+			m_socListener.Bind( ipLocal );
+			//start listening...
+			m_socListener.Listen (4);
+			// create the call back for any client connections...
+			m_socListener.BeginAccept(new AsyncCallback ( OnClientConnect),null);
+		}
+
+		public void Reset(){
+			//m_socWorker.Shutdown(SocketShutdown.Both);
+			if (m_socWorker.Connected){
+				m_socWorker.Close();
+			}
+			//start listening...
+			m_socListener.Listen (4);
+			// create the call back for any client connections...
+			m_socListener.BeginAccept(new AsyncCallback (OnClientConnect),null);
+		}
+
+		public void Send(byte[] byData){
+			m_socWorker.Send (byData);
+		}
+		public void Send(string data){
+			byte[] byData = System.Text.Encoding.ASCII.GetBytes(data + Environment.NewLine);
+			m_socWorker.Send (byData);
+		}
+			
+		private void OnClientConnect(IAsyncResult asyn)
+		{
+			try
+			{
+				m_socWorker = m_socListener.EndAccept (asyn);
+				ConnectionRequest(this);
+				WaitForData(m_socWorker);
+			}
+			catch(ObjectDisposedException)
+			{
+				logger.Info("Socket has been closed");
+				ConnectionClosed(this);
+			}
+			catch(SocketException se)
+			{
+				logger.FatalException("Socket error", se);
+				ConnectionClosed(this);
+			}
+			
+		}
+
+		private void WaitForData(System.Net.Sockets.Socket soc)
+		{
+			try
+			{
+				if  ( pfnWorkerCallBack == null )
+				{
+					pfnWorkerCallBack = new AsyncCallback (OnDataReceived);
+				}
+				CSocketPacket theSocPkt = new CSocketPacket ();
+				theSocPkt.thisSocket = soc;
+				// now start to listen for any data...
+				soc .BeginReceive (theSocPkt.dataBuffer ,0,theSocPkt.dataBuffer.Length ,SocketFlags.None,pfnWorkerCallBack,theSocPkt);
+			}
+			catch(SocketException se)
+			{
+				logger.FatalException("WaitForData exception", se);
+				ConnectionClosed(this);
+			}
+
+		}
+		
+		private void OnDataReceived(IAsyncResult asyn)
+		{
+			try
+			{
+				CSocketPacket theSockId = (CSocketPacket)asyn.AsyncState ;
+				//end receive...
+				int iRx  = 0 ;
+				iRx = theSockId.thisSocket.EndReceive (asyn);
+				char[] chars = new char[iRx +  1];
+				System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+				d.GetChars(theSockId.dataBuffer, 0, iRx, chars, 0);
+				System.String szData = new System.String(chars);
+				
+				Buffer= Buffer + szData.Trim('\0');
+				if (szData.Equals("\n\0") ){
+					DataArrival(this);
+					Buffer= "";
+				}
+				WaitForData(m_socWorker);
+			}
+			catch (ObjectDisposedException )
+			{
+				logger.Info("Socket has been closed");
+				ConnectionClosed(this);
+			}
+			catch(SocketException se)
+			{
+				logger.FatalException("Socket error", se);
+				ConnectionClosed(this);
+			}
+		}
+		
+		#region IDisposable Code
+
+		protected virtual void Dispose(bool disposing)
+      	{
+	        if (!disposed)
+	        {
+	            if (disposing)
+	            {
+	                // Free other state (managed objects).
+	            }
+	            // Free your own state (unmanaged objects).
+	            try{
+					logger.Trace("Dispose Executed");
+		            m_socListener.Shutdown(SocketShutdown.Both);
+		            m_socListener.Close();
+		            m_socWorker.Shutdown(SocketShutdown.Both);
+		            m_socWorker.Close();
+		            disposed = true;
+	            }catch (Exception ex){
+	            	logger.TraceException("Error Disposing", ex);
+	            }
+	        }
+		}
+
+		public void Dispose()
+		{
+		 Dispose(true);
+		 GC.SuppressFinalize(this);
+		}
+		// Use C# destructor syntax for finalization code.
+	    ~CSocket()
+	    {
+	        Dispose (false);
+	    }
+	    #endregion
+
+	}
+}
---
+++ trunk/Pop2Owa2/Pop2Owa/MainForm.cs
@@ -0,0 +1,171 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Carlos
+ * Date: 28/02/2010
+ * Time: 13:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+
+
+using NLog;
+
+namespace Pop2Owa
+
+{
+	[Serializable()]
+	public class ExchangeSettings
+	{
+		public string Server;
+		public string Domain;
+		public bool SaveOnSend;
+		public ExchangeVersion ServerVersion;
+		public string ProxyServer;
+		public string ProxyUser;
+		public string ProxyPassword;
+		public string ProxyDomain;
+	}
+	
+	/// <summary>
+	/// Class to hold persisted settings
+	/// </summary>
+	[Serializable()]
+	public class Settings: ExchangeSettings
+	{
+		public string HostIP;
+		public int Pop3Port;
+		public int SmtpPort;
+	}
+
+	
+
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+		internal static Settings pop2OwaSettings = new Settings();
+		internal static bool AuthRequired;
+		internal static bool ProxyConfigured= true;
+
+		private POP3Listener objPOP3;
+		private SMTPListener objSMTP;
+		
+		public MainForm()
+		{
+			try
+			{
+				logger.Trace("Calling InitializeComponent");
+				InitializeComponent();
+				logger.Trace("Calling LoadConfig");
+				LoadConfig();			
+				logger.Trace("Setting combos");	
+				cboVersion.DataSource = System.Enum.GetValues(typeof(ExchangeVersion));
+	            cboVersion.SelectedItem = ExchangeVersion.Exchange2007_SP1;
+			}
+			catch(Exception se)
+			{
+				logger.FatalException("Error loadding main form ", se);
+			}
+
+			try
+			{
+				logger.Trace("Setting sokects");
+				objPOP3 = new POP3Listener(IPAddress.Parse(pop2OwaSettings.HostIP), pop2OwaSettings.Pop3Port);
+				objSMTP = new SMTPListener(IPAddress.Parse(pop2OwaSettings.HostIP), pop2OwaSettings.SmtpPort);
+			}
+			catch(Exception se)
+			{
+				logger.FatalException("Error creating sockets ", se);
+			}
+		}
+
+		void BntApplyClick(object sender, EventArgs e)
+		{
+			SaveConfig();
+		}
+		
+		void LoadConfig()
+		{
+			try
+			{
+				// Get the isolated store for this assembly
+				IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForAssembly();
+	
+				// Open the settings file
+				FileStream flStream = new FileStream("config.xml", FileMode.Open, FileAccess.Read);
+    
+				// Deserialize the XML to an object
+				pop2OwaSettings = new Settings();
+				SoapFormatter SF = new SoapFormatter();
+				pop2OwaSettings = (Settings)SF.Deserialize(flStream);
+				flStream.Close();
+	
+				// And apply the settings to the form
+				txtServer.Text = pop2OwaSettings.Server;
+				txtDomain.Text = pop2OwaSettings.Domain;
+				chkSave.Checked= pop2OwaSettings.SaveOnSend;
+				cboVersion.SelectedItem= pop2OwaSettings.ServerVersion;
+				txtHostIP.Text=pop2OwaSettings.HostIP;
+				txtPop3Port.Text= pop2OwaSettings.Pop3Port.ToString();
+				txtSMTPPort.Text= pop2OwaSettings.SmtpPort.ToString();
+
+				txtProxyDomain.Text = pop2OwaSettings.ProxyDomain;
+				txtProxyServer.Text = pop2OwaSettings.ProxyServer;
+				txtProxyUser.Text = pop2OwaSettings.ProxyUser;
+				txtProxyPasword.Text = pop2OwaSettings.ProxyPassword;
+			
+			}catch(FileNotFoundException){
+				logger.Warn("Config File not found");
+			}catch(Exception ex){
+				logger.WarnException("Error Loading Config", ex);	
+			}
+		}
+
+		void SaveConfig()
+		{
+
+			// Create a settings object
+			pop2OwaSettings = new Settings();
+			pop2OwaSettings.Server = txtServer.Text;
+			pop2OwaSettings.Domain = txtDomain.Text;
+			pop2OwaSettings.ServerVersion= (ExchangeVersion) cboVersion.SelectedItem;
+			pop2OwaSettings.SaveOnSend = chkSave.Checked;
+			pop2OwaSettings.HostIP=txtHostIP.Text;
+			pop2OwaSettings.Pop3Port= int.Parse(txtPop3Port.Text);
+			pop2OwaSettings.SmtpPort= int.Parse(txtSMTPPort.Text);
+
+			pop2OwaSettings.ProxyServer = txtProxyServer.Text;
+			pop2OwaSettings.ProxyDomain = txtProxyDomain.Text;
+			pop2OwaSettings.ProxyUser = txtProxyUser.Text;
+			pop2OwaSettings.ProxyPassword = txtProxyPasword.Text;
+			// Create or truncate the settings file
+			// This will ensure that only the object we're
+			// saving right now will be in the file
+			FileStream flStream = new FileStream("config.xml", FileMode.Create , FileAccess.Write);
+
+			// Serialize the object to the file
+			SoapFormatter SF = new SoapFormatter();
+			SF.Serialize(flStream, pop2OwaSettings);
+			flStream.Close();
+		}
+		
+		void BntResetClick(object sender, EventArgs e)
+		{
+			SaveConfig();
+			objPOP3 = new POP3Listener(IPAddress.Parse(pop2OwaSettings.HostIP), pop2OwaSettings.Pop3Port);
+			objSMTP = new SMTPListener(IPAddress.Parse(pop2OwaSettings.HostIP), pop2OwaSettings.SmtpPort);
+
+		}
+	}
+
+}
